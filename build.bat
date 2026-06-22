bad9f116a5bb75862c544c6a1c6074834714b8e0
Alexander Traverso <nanaki1984@gmail.com>
2026-06-22 15:22:37 +0200

    built editor with game module, simple test with FolderNode

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+
+scons platform=windows debug_symbols=yes d3d12=yes profiler=tracy profiler_track_memory=yes profiler_record_on_demand=yes profiler_path=C:\Projects\GDGameOne\tracy prodution=yes accesskit=no angle=no use_pix=yes compiledb=yes
