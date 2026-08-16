0b8ccd96bae11cc32e1b4c77cd47498d48cfaed6
Alexander Traverso <nanaki1984@gmail.com>
2026-08-16 21:09:28 +0200

    Use custom.py to remove some unused modules, change bats accordingly

--- build_release_web.bat
+++ build_release_web.bat
@@ -2,4 +2,4 @@
 
 set EMSDK_QUIET=1
 call ..\emsdk\emsdk_env.bat
-scons platform=web target=template_release prodution=yes accesskit=no angle=no
+scons platform=web target=template_release d3d12=no prodution=yes disable_physics_2d=yes disable_navigation_2d=yes module_svg_enabled=no module_gltf_enabled=no
