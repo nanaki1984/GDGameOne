54262c1abd0a1bf6fe74bd1f752fcce1d28597c1
Alexander Traverso <nanaki1984@gmail.com>
2026-08-27 09:55:11 +0200

    Remove "deprecated = no" option from custom.py, it breaks the build (keep it in templates)

--- build_release_win.bat
+++ build_release_win.bat
@@ -1,3 +1,3 @@
 @echo off
 
-scons platform=windows target=template_release debug_symbols=yes profiler=tracy profiler_track_memory=yes profiler_record_on_demand=no profiler_path=C:\Projects\GDGameOne\tracy prodution=yes disable_physics_2d=yes disable_navigation_2d=yes module_svg_enabled=no module_gltf_enabled=no
+scons platform=windows target=template_release debug_symbols=yes profiler=tracy profiler_track_memory=yes profiler_record_on_demand=no profiler_path=C:\Projects\GDGameOne\tracy prodution=yes disable_physics_2d=yes disable_navigation_2d=yes module_svg_enabled=no module_gltf_enabled=no deprecated=no
